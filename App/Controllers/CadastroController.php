3ea43717eeb15fb21bf38c15dcff4809df5e3d2b
madiel <madielfranca@gmail.com>
2019-03-09 04:11:44 -0300

    cadastrar e selecionar

--- App/Controllers/CadastroController.php
+++ App/Controllers/CadastroController.php
@@ -2,6 +2,8 @@
 namespace App\Controllers;
 
 use Foundation\Controller;
+use App\Models\Pessoa;
+
 
 class CadastroController extends Controller
 {
@@ -12,7 +14,26 @@ class CadastroController extends Controller
 
     public function salvar()
     {
-        echo '<pre>';
-        var_dump($_POST);
+        // echo '<pre>';
+        // var_dump($_POST);
+
+        $dados = [
+            'nome' => input()->get('nome'),
+            'email' => input()->get('email')
+        ];
+
+        $pessoa = new Pessoa;
+
+        $qtd = $pessoa->insert($dados);
+
+        if ($qtd)
+        {
+            session()->put('_sucesso', 'incluido com sucesso');
+            return redirect()->route('cadastro.index');
+        }
+        session()->put('erro', 'erro ao excluir pessoa');
+        return redirect()->route('cadastro.index');
+
+
     }
 }
--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,11 +2,20 @@
 namespace App\Controllers;
 
 use Foundation\Controller;
+use App\Models\Pessoa;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return $this->render('index');
+        $pessoas = (new Pessoa)->getAll();
+        // echo '<pre>';
+        // var_dump($pessoas[0]->nome);
+        // foreach ($pessoas as $key => $pessoa){
+        //     var_dump($pessoas[$key]->nome);
+        // };
+        return $this->render('index', [
+            'pessoas' => $pessoas
+        ]);
     }
 }
